e7f37f281462dfd71ee00afb0196c24f90fddc94
ashish6667 <ay1060045@gmail.com>
2025-02-02 23:57:29 +0530

    updated header file

--- src/components1/Header.jsx
+++ src/components1/Header.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
+    
     return (
         <div className='py-7 px-10 bg-emerald-600 text-white flex item-center justify-between'>
             <h2 className='text-2xl'>Ashish</h2>
-            <div className='flex gap-10'>
-                  <a  className='text-xl underline' href='/'>Home</a>
-                  <a className='text-xl underline' href='/about'>About</a>
-                  <a  className='text-xl underline' href='/contact'>Contact</a>
-                  <a className='text-xl underline' href='/product'>Product</a>
+            <div className='flex gap-10 text-lg underline'>
+                 <Link to='/'>Home</Link>
+                 <Link to='/about'>About</Link>
+                 <Link to='/product'>Product</Link>
+                 <Link to='/contact'>Contact</Link>
             </div>
         </div>
     )
